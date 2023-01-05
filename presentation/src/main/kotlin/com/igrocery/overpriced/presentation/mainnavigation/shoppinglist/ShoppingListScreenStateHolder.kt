8bbcd7c250f296ca932f11f2d2b617498dbd24c6
mark <mark.slleung@gmail.com>
2023-01-05 18:12:57 +0800

    1. Start working on shopping list.

--- presentation/src/main/kotlin/com/igrocery/overpriced/presentation/mainnavigation/shoppinglist/ShoppingListScreenStateHolder.kt
+++ presentation/src/main/kotlin/com/igrocery/overpriced/presentation/mainnavigation/shoppinglist/ShoppingListScreenStateHolder.kt
@@ -1,4 +1,26 @@
 package com.igrocery.overpriced.presentation.mainnavigation.shoppinglist
 
-class ShoppingListScreenStateHolder {
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+
+class ShoppingListScreenStateHolder(savedState: List<*>? = null) {
+    // placeholder...
+}
+
+@Composable
+fun rememberShoppingListScreenState() = rememberSaveable(
+    stateSaver = listSaver(
+        save = {
+            listOf(
+                false
+            )
+        },
+        restore = { savedState ->
+            ShoppingListScreenStateHolder(savedState)
+        }
+    )
+) {
+    mutableStateOf(ShoppingListScreenStateHolder())
 }
